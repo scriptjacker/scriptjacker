a1a4276715636106489c97bf0d3428917b1d49da
Parth Narula <134507435+scriptjacker@users.noreply.github.com>
2023-09-08 05:08:37 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,18 @@ echo
 echo
 sleep 2
 apt update -y
+if [[ -z "$GOPATH" ]];then
+wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
+					sudo tar -xvf go1.13.4.linux-amd64.tar.gz
+					sudo mv go /usr/local
+					export GOROOT=/usr/local/go
+					export GOPATH=$HOME/go
+					export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+					echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
+					echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile			
+					echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile	
+					source ~/.bash_profile
+     fi
 apt install xargs -y
 apt install git -y
 apt install python-dnspython -y
